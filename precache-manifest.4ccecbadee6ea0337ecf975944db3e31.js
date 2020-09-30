be3bfd6f3d83c45bf0485f80676454d0d99ac1cf
jantepya <jantepya@gmail.com>
2020-09-29 22:34:22 -0700

    Updates

---
+++ precache-manifest.4ccecbadee6ea0337ecf975944db3e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5f05b3801a2076cb22b50675a63eda",
+    "url": "/Image-Sounder/index.html"
+  },
+  {
+    "revision": "0df0ac89383a5cd1a5b4",
+    "url": "/Image-Sounder/static/css/main.a117158e.chunk.css"
+  },
+  {
+    "revision": "3115d989797063650c0e",
+    "url": "/Image-Sounder/static/js/2.0a9c693f.chunk.js"
+  },
+  {
+    "revision": "039b7afb8563afca084f940d45b44d92",
+    "url": "/Image-Sounder/static/js/2.0a9c693f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df0ac89383a5cd1a5b4",
+    "url": "/Image-Sounder/static/js/main.19c3a66d.chunk.js"
+  },
+  {
+    "revision": "f68a2bc79da5e042156a",
+    "url": "/Image-Sounder/static/js/runtime-main.d9353990.js"
+  }
+]);
